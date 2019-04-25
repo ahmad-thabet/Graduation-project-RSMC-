32cc012f42b62872a3a7f04d7682dfd1a0c7a92d
memassamara <memassamara@gmail.com>
2019-04-25 03:36:23 +0300

    Added Patient Add/Show Appointment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,11 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './login/login.component';
-import {FormsModule} from '@angular/forms';
 import {AdminComponent} from './admin/admin.component';
 import {AdminMenuComponent} from './admin/admin-menu/admin-menu.component';
 import {AuthService} from './login/auth.service';
 import {PatientListComponent} from './admin/patient-list/patient-list.component';
 import {PatientProfileDetailsComponent} from './admin/patient-list/patient-profile-details/patient-profile-details.component';
 import {PatientCreateComponent} from './admin/patient-create/patient-create.component';
-import {CommonModule} from '@angular/common';
 import {AdminAppointmentsComponent} from './admin/admin-appointments/admin-appointments.component';
 import {AppointmentsListComponent} from './admin/admin-appointments/appointments-list/appointments-list.component';
 import {AppointmentsCreateComponent} from './admin/admin-appointments/appointments-create/appointments-create.component';
@@ -20,15 +15,12 @@ import {EmployeeCreateComponent} from './admin/employee-create/employee-create.c
 import {EmployeeListComponent} from './admin/employee-list/employee-list.component';
 import {AppointmentListComponent} from './admin/appointment-list/appointment-list.component';
 import {AppointmentCreateComponent} from './admin/appointment-create/appointment-create.component';
-import {CalendarModule} from 'primeng/calendar';
 import {SchaduleGeneratorComponent} from './admin/schadule-generator/schadule-generator.component';
 import {PatientEditDetailsComponent} from './admin/patient-list/patient-edit-details/patient-edit-details.component';
 import {PatientListFullComponent} from './admin/patient-list/patient-list-full/patient-list-full.component';
 import {EmployeeEditDetailsComponent} from './admin/employee-list/employee-edit-details/employee-edit-details.component';
 import {EmployeeListFullComponent} from './admin/employee-list/employee-list-full/employee-list-full.component';
 import {EmployeeProfileDetailsComponent} from './admin/employee-list/employee-profile-details/employee-profile-details.component';
-import {AccordionModule} from 'primeng/accordion';
-import {MenuItem} from 'primeng/api';
 import {ExceptionDateComponent} from './admin/exception-date/exception-date.component';
 import {VacationDateComponent} from './admin/vacation-date/vacation-date.component';
 import {InsuranceComponent} from './admin/insurance/insurance.component';
@@ -38,10 +30,23 @@ import {ClinicComponent} from './admin/clinic/clinic.component';
 import {ClinicCreateComponent} from './admin/clinic/clinic-create/clinic-create.component';
 import {ClinicListComponent} from './admin/clinic/clinic-list/clinic-list.component';
 import {ClinicDetailsComponent} from './admin/clinic/clinic-details/clinic-details.component';
-import { SchedulerModule } from '@progress/kendo-angular-scheduler';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
+import {SchedulerModule} from '@progress/kendo-angular-scheduler';
+import {PatientComponent} from './patient/patient.component';
+import {PatientMenuComponent} from './patient/patient-menu/patient-menu.component';
+import {AddAppointmentComponent} from './patient/add-appointment/add-appointment.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FlatpickrModule} from 'angularx-flatpickr';
+import {CalendarModule, DateAdapter} from 'angular-calendar';
+import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
+import {NgbModalModule} from '@ng-bootstrap/ng-bootstrap';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import 'flatpickr/dist/flatpickr.css';
+import {DlDateTimeDateModule, DlDateTimePickerModule} from "angular-bootstrap-datetimepicker";
 
 const appRoutes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -74,13 +79,19 @@ const appRoutes: Routes = [
         path: 'clinics', component: ClinicComponent, children: [
           {path: ':id', component: ClinicDetailsComponent}
         ]
-      }
+      },
     ]
   },
+  {
+    path: 'patient', component: PatientComponent, children: [
+      {path: 'add-appointment', component: AddAppointmentComponent},
 
+    ]
+  }
 ];
 
 @NgModule({
+
   declarations: [
     AppComponent,
     LoginComponent,
@@ -110,8 +121,10 @@ const appRoutes: Routes = [
     ClinicComponent,
     ClinicCreateComponent,
     ClinicListComponent,
-    ClinicDetailsComponent
-
+    ClinicDetailsComponent,
+    PatientComponent,
+    PatientMenuComponent,
+    AddAppointmentComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
@@ -119,12 +132,21 @@ const appRoutes: Routes = [
     FormsModule,
     CommonModule,
     HttpClientModule,
-    CalendarModule,
-    CommonModule,
     SchedulerModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    NgbModalModule,
+    ReactiveFormsModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    }),
+    DlDateTimeDateModule,  // <--- Determines the data type of the model
+    DlDateTimePickerModule,
   ],
-  providers: [PatinetSeviceService, AuthService],
+  exports: [AddAppointmentComponent],
+  providers: [PatinetSeviceService, AuthService, FormsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {
