3972eeb762196c5bb110e10d47f112de9a116f45
thabet Ahmad <ahmad.j.thabet@gmail.com>
2019-05-08 13:49:27 +0300

    admin appoin create edits

--- src/app/admin/appointment-create/appointment-create.component.ts
+++ src/app/admin/appointment-create/appointment-create.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {displayDate} from '../schadule-generator/events-utc';
-import {SchedulerEvent} from '@progress/kendo-angular-scheduler';
+import {ClinicDoctor} from '../../models/clinic-doctor.model';
+import {Clinic} from '../../models/clinic.model';
+import {Patient} from '../../models/patient.model';
 
 @Component({
   selector: 'app-appointment-create',
@@ -8,15 +9,36 @@ import {SchedulerEvent} from '@progress/kendo-angular-scheduler';
   styleUrls: ['./appointment-create.component.css']
 })
 export class AppointmentCreateComponent implements OnInit {
-  value: any;
+  selectedClinic: any;
+  selectedDoctor: any;
+  selectedDate: any;
+  selectedAppointment: any;
+  selectedPatient: any;
 
-  public selectedDate: Date = displayDate;
-  public events: SchedulerEvent[] = [];
+  allDoctors: ClinicDoctor[] = [];
+  allClinics: Clinic[] = [];
+  allPatients: Patient[] = [];
+  allAppointments: any[] = [new Date(), new Date(), new Date(), new Date(), new Date(),
+    new Date(), new Date(), new Date(), new Date(), new Date(), new Date(), new Date()];
+
+
+  /*  value: any;
+    public selectedDate: Date = displayDate;
+    public events: SchedulerEvent[] = [];*/
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  getCurrentModel() {
+    return JSON.stringify(this.selectedClinic + '-' + this.selectedDoctor + '-'
+      + this.selectedDate + '-' + this.selectedAppointment);
+  }
+
+  add_appointment(f) {
+    console.log(f);
   }
 
 }
