19a1ab7ecb0013533c4ef30a50c149955698b74f
thabet Ahmad <ahmad.j.thabet@gmail.com>
2019-05-27 16:46:43 +0300

    admin payment

--- src/app/admin/admin-payment/admin-payment.component.ts
+++ src/app/admin/admin-payment/admin-payment.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Paymentdetails} from '../../models/paymentdetails.model';
 import {PaymentServiceService} from '../../service/payment-service.service';
+import {Patient} from '../../models/patient.model';
+import {PatinetSeviceService} from '../../service/patinet-sevice.service';
 
 @Component({
   selector: 'app-admin-payment',
@@ -9,6 +11,9 @@ import {PaymentServiceService} from '../../service/payment-service.service';
 })
 export class AdminPaymentComponent implements OnInit {
 
+  fromdate = new Date();
+  todate = new Date();
+  patient = '';
 
   details: Paymentdetails[] = [
     new Paymentdetails('401110796', '1', '100', '01-01-2019', 'Cash', '1'),
@@ -20,12 +25,15 @@ export class AdminPaymentComponent implements OnInit {
     new Paymentdetails('850038746', '1', '200', '07-07-2019', 'Cash', '7'),
   ];
   error = '';
+  patients: Patient[] = [];
 
-  constructor(private paymentService: PaymentServiceService) {
+  constructor(private paymentService: PaymentServiceService,
+              private patinetService: PatinetSeviceService) {
   }
 
   ngOnInit() {
     this.loaddetails();
+    this.loadpatient();
   }
 
   private loaddetails() {
@@ -39,4 +47,29 @@ export class AdminPaymentComponent implements OnInit {
       }
     );
   }
+
+  dateChanged() {
+
+  }
+
+  onSelectPatient(id: number) {
+
+  }
+
+  private loadpatient() {
+    this.patinetService.get_patient().subscribe(
+      (res: Patient[]) => {
+        this.patients = res;
+        console.log(res);
+      },
+      (err) => {
+        this.error = err;
+      }
+    );
+  }
+
+  getCurrentModel() {
+    return JSON.stringify(this.patient);
+  }
+
 }
