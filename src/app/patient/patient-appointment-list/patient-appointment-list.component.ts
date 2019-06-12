46974aca04e3573f4b2bbb1acb304df6d0da533c
thabet Ahmad <ahmad.j.thabet@gmail.com>
2019-06-12 18:39:05 +0300

    patient appointment list

--- src/app/patient/patient-appointment-list/patient-appointment-list.component.ts
+++ src/app/patient/patient-appointment-list/patient-appointment-list.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Appointment} from '../../models/appointment.model';
+import {ClinicDoctor} from '../../models/clinic-doctor.model';
+import {Clinic} from '../../models/clinic.model';
+import {Employee} from '../../models/employee.model';
+import {Schadule} from '../../models/schedule.model';
+import {ClinicServiceService} from '../../service/clinic-service.service';
+import {AppoinmentServiceService} from '../../service/appoinment-service.service';
+import {PatinetSeviceService} from '../../service/patinet-sevice.service';
+import {EmployeeServiceService} from '../../service/employee-service.service';
+import {ScheduleServiceService} from '../../service/schedule-service.service';
+import {MessageService} from 'primeng/api';
+import {PaymentServiceService} from '../../service/payment-service.service';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {AuthService} from '../../service/auth.service';
 
 @Component({
   selector: 'app-patient-appointment-list',
@@ -7,9 +21,114 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientAppointmentListComponent implements OnInit {
 
-  constructor() { }
+  doctorID = '';
+  error = '';
+  success = '';
+
+  fromdate = new Date();
+  todate = new Date();
+
+  selectedClinic: any;
+  selectedDoctor: any;
+  appointments: Appointment[] = [];
+  filteredAppointment: Appointment[] = [];
+
+  doctors: ClinicDoctor[];
+  doctorss: ClinicDoctor[];
+
+  clinics: Clinic[];
+
+  doctorsID: Employee[] = [];
+  doctorsIDs: Employee[] = [];
+
+  doctorsSchedule: Schadule[] = [];
+  doctorsSchedules: Schadule[] = [];
+
+  patientID = 0;
+
+  constructor(private clinicService: ClinicServiceService,
+              private appointmentService: AppoinmentServiceService,
+              private patinetservice: PatinetSeviceService,
+              private employeeService: EmployeeServiceService,
+              private scheduleService: ScheduleServiceService,
+              private messageService: MessageService,
+              private paymentService: PaymentServiceService,
+              private modalService: NgbModal,
+              private authService: AuthService) {
+  }
 
   ngOnInit() {
+    if (this.authService.isAuth()) {
+      this.patientID = +this.authService.getUserId();
+    }
+    this.loaddoctors();
+    this.loadalldoctors();
+    this.loadAppointmant();
+    this.loadClinics();
+
+    setInterval(() => {
+      this.loadAppointmant();
+    }, 1000000);
+  }
+
+  private loadClinics() {
+    this.clinicService.get_clinic().subscribe(
+      (res: Clinic[]) => {
+        this.clinics = res;
+        console.log(res);
+      },
+      (err) => {
+        this.error = err;
+      }
+    );
+  }
+
+  private loadalldoctors() {
+    this.clinicService.get_clinic_doctors().subscribe(
+      (res: ClinicDoctor[]) => {
+        this.doctors = res;
+        console.log('all docs ' + this.doctors.toString());
+      },
+      (err) => {
+        this.error = err;
+      }
+    );
+  }
+
+  private loadAppointmant() {
+    this.appointmentService.get_appointment().subscribe(
+      (res: Appointment[]) => {
+        this.appointments = res;
+      },
+      (err) => {
+        this.error = err;
+      }, () => {
+        this.filteredAppointment = this.appointments.filter(x => x.patientID === this.patientID);
+      }
+    );
+  }
+
+  private loaddoctors() {
+    this.employeeService.get_employee().subscribe(
+      (res: Employee[]) => {
+        this.doctorsID = res;
+      },
+      (err) => {
+        this.error = err;
+      }
+    );
+  }
+
+  getClinicName(value: any) {
+    const k = this.clinics.findIndex(x => x.clinicID === value);
+    if (k !== -1) {
+      return JSON.stringify(this.clinics[k].clinicname);
+    }
+  }
+
+  dateChanged() {
+    // TODO: implement this function
+    // get appointments by date
   }
 
 }
